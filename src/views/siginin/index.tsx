ea1ec3480b3c0105ff93e44bdd326836960eaa91
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-07-25 14:55:26 +0530

    updated signin page

--- src/views/siginin/index.tsx
+++ src/views/siginin/index.tsx
@@ -9,7 +9,8 @@ export default function Signin() {
     if (token) {
       navigate("/dashboard");
     }
-  }, [navigate]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className=" w-fit shadow-xl rounded-xl mx-auto py-5  mt-11 p-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h1 className="text-center text-3xl font-semibold mb-6 mt-3 text-white">
